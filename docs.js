007bdd7098b13d8446061e0297621c916c9a2980
Ian Park <asbear@gmail.com>
2016-11-07 15:44:14 +0000

    aa

---
+++ docs.js
@@ -0,0 +1,45 @@
+var app = angular.module("docs", []);
+
+app.filter('searchFor', function(){
+
+	// All filters must return a function. The first parameter
+	// is the data that is to be filtered, and the second is an
+	// argument that may be passed with a colon (searchFor:searchString)
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.title.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
+
+});
+
+function DocsController($scope) {
+   $scope.items = [
+   {
+      url: "/files/links.ppsx",
+      title: "PPT with links"
+   },
+   {
+      url: "http://www.spicytan.com/Chinese.pdf",
+      title: "Chinese (Spicytan)"
+   }
+   ];
+}
+
